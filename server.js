f40755980bada964fd22a6474f4e16a074e28650
peer100 <Harbordpeer@gmail.com>
2025-06-07 22:08:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const app = express();
-
-// ⛳️ Render gibt den Port über process.env.PORT vor
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Beispiel-Endpunkte
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
